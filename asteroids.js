6aa2811cfe5472d5b09f0e8c2c0efdf5739b0bb6
piyushpradhan <piyushpradhan3.14@gmail.com>
2020-10-02 15:22:47 +0530

    asteroids:

--- asteroids.js
+++ asteroids.js
@@ -9,7 +9,6 @@ let asteroids = [];
 let score = 0;
 let lives = 3;
 
-// HOMEWORK SOLUTION - Contributed by luckyboysunday
 let highScore;
 let localStorageName = "HighScore";
 
@@ -42,7 +41,6 @@ function SetupCanvas(){
     document.body.addEventListener("keydown", HandleKeyDown);
     document.body.addEventListener("keyup", HandleKeyUp);
 
-    // HOMEWORK SOLUTION - Contributed by luckyboysunday
     // Retrieves locally stored high scores
     if (localStorage.getItem(localStorageName) == null) {
         highScore = 0;
@@ -53,9 +51,6 @@ function SetupCanvas(){
     Render();
 }
 
-// HOMEWORK SOLUTION
-// Move event handling functions so that we can turn off
-// event handling if game over is reached
 function HandleKeyDown(e){
     keys[e.keyCode] = true;
 }
@@ -87,19 +82,12 @@ class Ship {
         this.angle += this.rotateSpeed * dir;
     }
     Update() {
-        // Get current direction ship is facing
         let radians = this.angle / Math.PI * 180;
-
-        // If moving forward calculate changing values of x & y
-        // If you want to find the new point x use the 
-        // formula oldX + cos(radians) * distance
-        // Forumla for y oldY + sin(radians) * distance
         if (this.movingForward) {
             this.velX += Math.cos(radians) * this.speed;
             this.velY += Math.sin(radians) * this.speed;
         }
-        // If ship goes off board place it on the opposite
-        // side    
+
         if (this.x < this.radius) {
             this.x = canvas.width;
         }
@@ -112,19 +100,15 @@ class Ship {
         if (this.y > canvas.height) {
             this.y = this.radius;
         }
-        // Slow ship speed when not holding key
         this.velX *= 0.99;
         this.velY *= 0.99;
 
-        // Change value of x & y while accounting for
-        // air friction    
         this.x -= this.velX;
         this.y -= this.velY;
     }
     Draw() {
         ctx.strokeStyle = this.strokeColor;
         ctx.beginPath();
-        // Angle between vertices of the ship
         let vertAngle = ((Math.PI * 2) / 3);
 
         let radians = this.angle / Math.PI * 180;
@@ -221,56 +205,41 @@ function CircleCollision(p1x, p1y, r1, p2x, p2y, r2){
     }
 }
 
-// Handles drawing life ships on screen
 function DrawLifeShips(){
     let startX = 1350;
     let startY = 10;
     let points = [[9, 9], [-9, 9]];
-    ctx.strokeStyle = 'white'; // Stroke color of ships
-    // Cycle through all live ships remaining
+    ctx.strokeStyle = 'white'; 
     for(let i = 0; i < lives; i++){
-        // Start drawing ship
         ctx.beginPath();
-        // Move to origin point
         ctx.moveTo(startX, startY);
-        // Cycle through all other points
         for(let j = 0; j < points.length; j++){
             ctx.lineTo(startX + points[j][0], 
                 startY + points[j][1]);
         }
-        // Draw from last point to 1st origin point
         ctx.closePath();
-        // Stroke the ship shape white
         ctx.stroke();
-        // Move next shape 30 pixels to the left
         startX -= 30;
     }
 }
 
 function Render() {
-    // Check if the ship is moving forward
     ship.movingForward = (keys[87]);
 
     if (keys[68]) {
-        // d key rotate right
         ship.Rotate(1);
     }
     if (keys[65]) {
-        // a key rotate left
        ship.Rotate(-1);
     }
    
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
-    // Display score
     ctx.fillStyle = 'white';
     ctx.font = '21px Arial';
     ctx.fillText("SCORE : " + score.toString(), 20, 35);
 
-    // If no lives signal game over
     if(lives <= 0){
-        // HOMEWORK SOLUTION
-        // If Game over remove event listeners to stop getting keyboard input
         document.body.removeEventListener("keydown", HandleKeyDown);
         document.body.removeEventListener("keyup", HandleKeyUp);
 
@@ -280,7 +249,6 @@ function Render() {
         ctx.fillText("GAME OVER", canvasWidth / 2 - 150, canvasHeight / 2);
     }
 
-    // HOME WORK SOLUTION : Creates a new level and increases asteroid speed
     if(asteroids.length === 0){
         ship.x = canvasWidth / 2;
         ship.y = canvasHeight / 2;
@@ -293,10 +261,8 @@ function Render() {
         }
     }
 
-    // Draw life ships
     DrawLifeShips();
 
-    // Check for collision of ship with asteroid
     if (asteroids.length !== 0) {
         for(let k = 0; k < asteroids.length; k++){
             if(CircleCollision(ship.x, ship.y, 11, asteroids[k].x, asteroids[k].y, asteroids[k].collisionRadius)){
@@ -309,13 +275,11 @@ function Render() {
         }
     }
 
-    // Check for collision with bullet and asteroid
     if (asteroids.length !== 0 && bullets.length != 0){
 loop1:
         for(let l = 0; l < asteroids.length; l++){
             for(let m = 0; m < bullets.length; m++){
                 if(CircleCollision(bullets[m].x, bullets[m].y, 3, asteroids[l].x, asteroids[l].y, asteroids[l].collisionRadius)){
-                    // Check if asteroid can be broken into smaller pieces
                     if(asteroids[l].level === 1){
                         asteroids.push(new Asteroid(asteroids[l].x - 5, asteroids[l].y - 5, 25, 2, 22));
                         asteroids.push(new Asteroid(asteroids[l].x + 5, asteroids[l].y + 5, 25, 2, 22));
@@ -326,9 +290,6 @@ loop1:
                     asteroids.splice(l,1);
                     bullets.splice(m,1);
                     score += 20;
-
-                    // Used to break out of loops because splicing arrays
-                    // you are looping through will break otherwise
                     break loop1;
                 }
             }
@@ -355,8 +316,6 @@ loop1:
         }
     }
 
-    // HOMEWORK SOLUTION
-    // Updates the high score using local storage
     highScore = Math.max(score, highScore);
     localStorage.setItem(localStorageName, highScore);
     ctx.font = '21px Arial';
